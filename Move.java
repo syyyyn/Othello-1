28ccef434fa7a4a78cd8339bcf5b57043c2f39e7
pe4ro <pedro.munoz.garcia@alumnos.upm.es>
2014-05-05 19:49:52 +0200

    Create Move.java

---
+++ Move.java
@@ -0,0 +1,9 @@
+public class Move { // TURN??
+        int i, j;
+        public Move () {
+        }
+        public Move (int i, int j) {
+                this.i = i;
+                this.j = j;
+        }
+};
