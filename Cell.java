ca468771651fac82fca4d0db6a6e9f7ff3ff9442
pe4ro <pedro.munoz.garcia@alumnos.upm.es>
2014-05-07 17:42:58 +0200

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,13 +1,19 @@
 /**
  * Created by pmunoz on 01/04/14.
+ * Updated by aeap and jgeorge on 05/05/14.
  */
 public class Cell {
-    private static final char BLACK = 'X';
-    private static final char WHITE = 'O';
+    public static final char BLACK = 'X'; //
+    public static final char WHITE = 'O'; // 
+    
+    public static final char CANSELECT = '?';
 
-    private boolean empty;
-
-    private int value;
+    public boolean empty; //
+    public boolean white;
+    public boolean black;
+    public boolean canselect;
+    
+    public int value; // 0 - white, 1 - black
 
     public Cell() {
         this.empty = true;
@@ -20,18 +26,50 @@ public class Cell {
     public void placeChip( int player ) {
         this.empty = false;
         this.value = player;
+        
+        if(player == 0){
+        	this.white = true;
+        	this.black = false;
+        }
+        
+        else{
+        	this.black = true;
+        	this.white = false;
+        }
+    }
+
+    public void changeChip() {
+        placeChip((value+1)%2);
+    }
+
+    public boolean isWhite(){
+    	return this.white;
+    }
+    
+    public boolean isBlack(){
+    	return this.black;
+    }
+    
+    public boolean canSelect() {
+        return this.canselect;
+    }
+    
+    public void setSelect() {
+        this.canselect = true;
     }
 
     public void display() {
        if (this.isEmpty())
        {
-            System.out.print("| " + " " + " |");
+            System.out.print("[ " + " " + " ]");
        }
        else {
            char content = BLACK;
            if (this.value == 0)
                content = WHITE;
-           System.out.print("| " + content + " |");
+           if (canselect)
+               content = CANSELECT;
+           System.out.print("[ " + content + " ]");
        }
     }
 }
