347cc8c92f3b0532102e4b379937b02cf1d2ecb8
pe4ro <pedro.munoz.garcia@alumnos.upm.es>
2014-05-09 20:50:11 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,7 +1,4 @@
-package othello.Othello;
-import java.util.ArrayList;
 
-public class Player {
     private String name;
     private int color;
     private Board board;
@@ -12,11 +9,21 @@ public class Player {
         this.board = board;
     }
 
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getColor() {
+        return this.color;
+    }
 
     public void placeChip(int row, int col) {
-
         this.board.placeChip( this.color, row, col );
+        
+        Move move = new Move(row, col);
+        this.board.replaceChip(move, this.color);
     }
+    
     public void findCanSelect(){
 
         ArrayList<Move> moves = board.validMove(this.color);
@@ -28,4 +35,3 @@ public class Player {
             board.setCanSelect(move);
     }
 
-}
